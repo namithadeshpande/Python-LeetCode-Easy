4ce9f28e204456a1c57b247ba1c36b8b55efd5ff
namithadeshpande <namithadeshpande@gmail.com>
2025-06-12 23:35:04 -0500

    solution added

---
+++ containsDuplicates.py
@@ -0,0 +1,13 @@
+# 217. Contains Duplicate
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        nums.sort()
+        for i in range(1, len(nums)):
+            if nums[i]== nums[i-1]:
+                return True
+
+        return False
